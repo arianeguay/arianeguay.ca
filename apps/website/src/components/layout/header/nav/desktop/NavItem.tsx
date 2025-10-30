9af20402d12766606dca9026cbdb1fc1b105269a
Ariane Guay <ariane.dguay@gmail.com>
2025-10-30 11:47:20 -0400

    bugfix : home page styling when active

--- apps/website/src/components/layout/header/nav/desktop/NavItem.tsx
+++ apps/website/src/components/layout/header/nav/desktop/NavItem.tsx
@@ -19,6 +19,9 @@ interface NavItemProps extends NavItem {
 const getFullHandle = (page: Page, locale: string) => {
   const parentSlug: string | undefined = page?.parentPage?.slug;
 
+  if (page.slug === "home") {
+    return locale === "fr" ? "/" : "/en";
+  }
   const href = [locale !== "fr" && "en", parentSlug, page.slug]
     .filter(Boolean)
     .join("/");
